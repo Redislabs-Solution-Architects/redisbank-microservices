8a574112749836f34d875f43f5fb91d92716dd0e
Lars Rosenquist <rosenql@gmail.com>
2022-09-20 13:38:08 +0200

    Added comment on TLS config Removed unused repository

--- redisbank-transactions/src/main/java/com/redis/demos/redisbanktransactions/BankTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.redis.demos.redisbanktransactions;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BankTransactionRepository extends CrudRepository<BankTransaction, String>{
-    
-}
