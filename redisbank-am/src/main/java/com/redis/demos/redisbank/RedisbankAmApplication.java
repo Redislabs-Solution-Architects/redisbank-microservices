a7edf03345976de38a7ac2e7f8577067e2ae0c93
Lars Rosenquist <rosenql@gmail.com>
2022-09-22 10:23:37 +0200

    am: fixed profile switching tls/no tls added testcontainers support

--- redisbank-am/src/main/java/com/redis/demos/redisbank/RedisbankAmApplication.java
+++ redisbank-am/src/main/java/com/redis/demos/redisbank/RedisbankAmApplication.java
@@ -1,13 +1,9 @@
 package com.redis.demos.redisbank;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.redis.spring.lettucemod.RedisModulesAutoConfiguration;
-
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = RedisModulesAutoConfiguration.class)
 public class RedisbankAmApplication {
 
 	public static void main(String[] args) {
--- redisbank-am/src/test/java/com/redis/demos/redisbank/redisbankam/RedisbankAmApplicationTests.java
+++ redisbank-am/src/test/java/com/redis/demos/redisbank/redisbankam/RedisbankAmApplicationTests.java
@@ -2,9 +2,31 @@ package com.redis.demos.redisbank.redisbankam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class RedisbankAmApplicationTests {
+
+	static final String REDIS_IMAGE = "redis/redis-stack-server:latest";
+	static final int REDIS_PORT = 6379;
+
+	@Container
+	static GenericContainer<?> redisContainer = new GenericContainer<>(DockerImageName.parse(REDIS_IMAGE))
+			.withExposedPorts(REDIS_PORT);
+
+	@DynamicPropertySource
+	static void redisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.redis.host", redisContainer::getContainerIpAddress);
+		registry.add("spring.redis.port", redisContainer::getFirstMappedPort);
+		registry.add("spring.redis.host-tr", redisContainer::getContainerIpAddress);
+		registry.add("spring.redis.port-tr", redisContainer::getFirstMappedPort);
+	}
 
 	@Test
 	void contextLoads() {
